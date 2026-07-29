751f2c9c3067d116a7812395d9b0113cc4872f82
Shenna Risqianto Wilfred Piri <shenna.piri@fourenergy.de>
2026-07-29 10:40:27 +0200

    add ui

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    fetch('/api/targets')
+      .then(res => res.text())
+      .then(setData)
+  }, [])
+
+  return <h1>{data}</h1>
+}
+
+export default App
