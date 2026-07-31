b0e77e8e430f42cdf27dc8e2a44816f9e437ad86
Shenna Risqianto Wilfred Piri <shenna.piri@fourenergy.de>
2026-07-31 13:03:04 +0200

    update name

---
+++ internal/database/migrations/sqlite/000006_update_target_names.up.sql
@@ -0,0 +1,4 @@
+UPDATE targets SET name = 'Hetzner Staging' WHERE name = 'CV Staging' OR url LIKE '%shennarwp.com/cv-staging%';
+UPDATE targets SET name = 'Azure Container App' WHERE name = 'Azure Resume' OR url LIKE '%azurecontainerapps.io%';
+UPDATE targets SET name = 'Azure Static Web' WHERE name = 'Azure Static App' OR url LIKE '%azurestaticapps.net%';
+UPDATE targets SET name = 'Oracle Prod' WHERE name = 'Shenna Piri' OR url LIKE '%shenna.rwpiri.com%';
