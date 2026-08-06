8bf0e54ee860f7c9518200e0d3cc570a425f98de
Shenna Risqianto Wilfred Piri <shenna.piri@fourenergy.de>
2026-08-06 13:05:42 +0200

    show response time in seconds above 999ms

--- web/src/utils/format.ts
+++ web/src/utils/format.ts
@@ -1,5 +1,5 @@
 export function formatResponseTime(ms: number): string {
-  if (ms <= 9999) return `${ms}ms`;
+  if (ms <= 999) return `${ms}ms`;
   const seconds = ms / 1000;
   return `${Math.round(seconds * 100) / 100}s`;
 }
